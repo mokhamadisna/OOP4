b00e59ae97519f54541b6733d55fbb499b35cb8a
mokhamadisna <mokhammadisna@gmail.com>
2020-11-17 11:45:41 +0700

    is

---
+++ src/com/isna/percabangan/IfThenElse.java
@@ -0,0 +1,12 @@
+package com.isna.percabangan;
+
+public class IfThenElse {
+    public static void main(String[] args) {
+        boolean isOn = false;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        } else {
+            System.out.println("Kondisi tidak terpenuhi...");
+        }
+    }
+}
